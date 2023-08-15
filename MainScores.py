30b5f0f074890392485695e6fc4f324a8f0a5493
Roy Goren <roygorenxd2gmail.com>
2023-08-15 17:34:08 +0300

    MainScore.py revert

--- MainScores.py
+++ MainScores.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def read_score():
     try:
-        # scores_open = open(SCORES_FILE_NAME, 'r')
-        # scores_content = int(scores_open.read())
-        scores_content = 8
+        scores_open = open(SCORES_FILE_NAME, 'r')
+        scores_content = int(scores_open.read())
         return render_template('WebScore.html', SCORE=scores_content)
     except ValueError:
         return render_template('WebError.html', ERROR=BAD_RETURN_CODE)
