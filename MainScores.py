b6349ef358c3043de6ec18d2b725d12927dc270b
Roy Goren <roygorenxd2gmail.com>
2023-08-15 16:48:21 +0300

    test for different open type

--- MainScores.py
+++ MainScores.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def read_score():
     try:
-        scores_open = open(SCORES_FILE_NAME, 'r')
-        scores_content = int(scores_open.read())
+        # scores_open = open(SCORES_FILE_NAME, 'r')
+        # scores_content = int(scores_open.read())
+        scores_content = 8
         return render_template('WebScore.html', SCORE=scores_content)
     except ValueError:
         return render_template('WebError.html', ERROR=BAD_RETURN_CODE)
